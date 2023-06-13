7087ea4d19883c16967f075e5b5c9686eefe5084
Fakhriddin <fakhmirakov1998@gmail.com>
2023-06-13 10:14:09 +0500

    lesson 2

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-let slides = document.querySelectorAll('.slide')
+let item = document.querySelector('.item')
+let placeholders = document.querySelectorAll('.placeholder')
 
-slides.forEach(slide => {
-  slide.onclick = () => {
-    slides.forEach(slide => slide.classList.remove('active'))
-    slide.classList.add('active')
+item.ondragstart = (e) => {
+  setTimeout(() => {
+    e.target.classList.add('hide')
+  }, 0);
+}
+
+item.ondragend = (e) => {
+  e.target.classList.remove('hide')
+}
+
+for (let placeholder of placeholders) {
+  placeholder.ondragover = (e) => {
+    e.preventDefault()
   }
-})
+  placeholder.ondragenter = (e) => {
+    e.target.classList.add('hovered')
+  }
+  placeholder.ondragleave = (e) => {
+    e.target.classList.remove('hovered')
+  }
+  placeholder.ondrop = (e) => {
+    e.target.append(item)
+    e.target.classList.remove('hovered')
+  }
+}
